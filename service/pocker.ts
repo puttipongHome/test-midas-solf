c1b5b6c525e5859d5d77f9f542ff90f1a4bcfb6d
Home <homehome12589@hotmail.com>
2023-05-26 09:58:32 +0700

    update

---
+++ service/pocker.ts
@@ -0,0 +1,20 @@
+const pocker = {
+  patient: async (data: any) => {
+    const results = data;
+    return results;
+  },
+};
+
+const slave = {
+  patient: async (data: any) => {
+    const results = data;
+    return results;
+  },
+};
+
+const controller = {
+  pocker,
+  slave,
+};
+
+export default controller;
